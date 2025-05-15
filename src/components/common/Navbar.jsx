7ae4557a9688f330178a450dc3bf857d66eda387
Invisible-CAT25 <gargjatin2526@gmail.com>
2025-05-15 17:39:58 +0530

    responsiveness added

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -14,7 +14,7 @@ function Navbar() {
     <div
       className={`flex h-18 items-center justify-center transition-all duration-200`}
     >
-      <div className="flex w-11/12 max-w-[1260px] items-center justify-between">
+      <div className="flex w-11/12 items-center justify-between">
         {/* Logo */}
         <Link to="/">
           <img src={logo} alt="Logo" width={160} height={32} loading="lazy" />
