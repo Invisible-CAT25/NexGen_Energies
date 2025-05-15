311c8e3948ef953c4fc9dfdb83055d466fef25c0
Invisible-CAT25 <gargjatin2526@gmail.com>
2025-05-15 11:18:44 +0530

    Project Page Added

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Navbar from './components/common/Navbar'
 import Home from './pages/Home'
 import About from './pages/About'
 import Contact from './pages/Contact'
+import Projects from './pages/Projects'
 
 function App() {
 
@@ -19,8 +20,8 @@ function App() {
         <Route path='/' element={<Home />}  />
         <Route path='/about-us' element={<About />} />
         <Route path='/contact-us' element={<Contact />} />
+        <Route path='/projects' element={<Projects />} />
         
-
 
 
       </Routes>
---
+++ src/pages/Projects.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import ProjectHeroSection from '../components/core/ProjectPage/ProjectHeroSection'
+import Project from '../components/core/ProjectPage/Project'
+import Map from '../components/core/ProjectPage/Map'
+import Footer from '../components/common/Footer'
+
+const Projects = () => {
+  return (
+    <div>
+        <ProjectHeroSection />
+
+        <Project />
+        <Project active={true} />
+        <Project />
+        <Project active={true} />
+
+        <Map />
+        <Footer />
+
+    </div>
+  )
+}
+
+export default Projects
